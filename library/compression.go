987c14eda46a99a4f3a79e23bfa273c618bdd932
Ben V. Brown <Ralim@Ralimtek.com>
2021-12-22 08:13:27 +1100

    Compression

--- library/compression.go
+++ library/compression.go
@@ -1,8 +1,66 @@
 package library
 
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/ralim/switchhost/utilities"
+	"github.com/rs/zerolog/log"
+)
+
 // Compression handles compressing files using the existingnsz tooling
 // It runs a single file compression at a time in the background
 
 func (lib *Library) compressionWorker() {
 	//Dequeue any requests off the queue and run the compression
+
+	for request := range lib.fileCompressionRequests {
+		//For each requested file, run it through NSZ and check output
+		err := lib.NSZCompressFile(request)
+		if err != nil {
+			log.Err(err).Msg("NSZ compression failed")
+		} else {
+			// Check if the source file has been deleted
+			// Check if the expected output file is made
+
+			if !utilities.Exists(request) {
+				//Source file has been deleted, notify library
+				event := &scanRequest{
+					path:             request,
+					isEndOfStartScan: false,
+					isNotifierBased:  true,
+					fileRemoved:      true,
+				}
+				lib.fileScanRequests <- event
+			}
+			//Figure out the output file path
+			newpath := request[0:len(request)-1] + "z"
+			if utilities.Exists(newpath) {
+				//Source file has been deleted, notify library
+				event := &scanRequest{
+					path:             newpath,
+					isEndOfStartScan: false,
+					isNotifierBased:  true,
+					fileRemoved:      false,
+				}
+				lib.fileScanRequests <- event
+			}
+
+		}
+	}
+}
+
+func (lib *Library) NSZCompressFile(path string) error {
+	//Call out to external tool using the user provided base string
+	parts := strings.Split(lib.settings.NSZCommandLine, " ")
+	cleanedParts := []string{}
+	for _, p := range parts {
+		if len(p) > 0 {
+			cleanedParts = append(cleanedParts, p)
+		}
+	}
+	cleanedParts = append(cleanedParts, path)
+	cmd := exec.Command(cleanedParts[0], cleanedParts[1:]...)
+	return cmd.Run()
+
 }
---
+++ library/compression_test.go
@@ -0,0 +1,94 @@
+package library
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/ralim/switchhost/settings"
+)
+
+func TestNSZCompressFile(t *testing.T) {
+	t.Parallel()
+	tempFile, err := os.CreateTemp("", "TestNSZCompressFile-*")
+	if err != nil {
+		t.Error(err)
+	}
+	tempFile.WriteString("Test")
+	tempFile.Close()
+	defer os.Remove(tempFile.Name())
+	sett := settings.Settings{
+		NSZCommandLine: "cat ",
+	}
+	lib := Library{
+		settings: &sett,
+	}
+	err = lib.NSZCompressFile(tempFile.Name())
+	if err != nil {
+		t.Error("Should run normal commands fine", err)
+	}
+	sett.NSZCommandLine = "kfkjhksjhfksjhfksjdhfksjdfhksjhfdksdjhfksjhfksjdhfksdhfksjfd"
+	err = lib.NSZCompressFile(tempFile.Name())
+	if err == nil {
+		t.Error("Should throw error on bad ")
+	}
+	sett.NSZCommandLine = "cat //"
+	err = lib.NSZCompressFile(tempFile.Name())
+	fmt.Println(err)
+	if err == nil {
+		t.Error("Should throw error on bad ")
+	}
+}
+
+func TestCompressionWorker(t *testing.T) {
+	t.Parallel()
+	tempFile, err := os.CreateTemp("", "TestNSZCompressFile-*")
+	if err != nil {
+		t.Error(err)
+	}
+	tempFile.WriteString("Test")
+	tempFile.Close()
+	defer os.Remove(tempFile.Name())
+	sett := settings.Settings{
+		NSZCommandLine: "rm",
+	}
+	lib := Library{
+		settings:                &sett,
+		fileCompressionRequests: make(chan string),
+		fileScanRequests:        make(chan *scanRequest),
+	}
+
+	defer close(lib.fileCompressionRequests)
+	defer close(lib.fileScanRequests)
+
+	go lib.compressionWorker()
+
+	lib.fileCompressionRequests <- tempFile.Name()
+
+	result := <-lib.fileScanRequests
+	if result == nil {
+		t.Error("bad result")
+	} else {
+		if !result.fileRemoved {
+			t.Error("should report file removed")
+		}
+		if result.path != tempFile.Name() {
+			t.Error("should report file path")
+		}
+	}
+	//Test the made new file path
+	lib.settings.NSZCommandLine = "touch"
+	lib.fileCompressionRequests <- "/tmp/testingFiles.nsz"
+
+	result = <-lib.fileScanRequests
+	if result == nil {
+		t.Error("bad result")
+	} else {
+		if result.fileRemoved {
+			t.Error("should report file created")
+		}
+		if result.path != "/tmp/testingFiles.nsz" {
+			t.Error("should report file path")
+		}
+	}
+}
