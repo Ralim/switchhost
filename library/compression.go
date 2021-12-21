7a878365f5c5823da1271c4d015a077d1aa43747
Ben V. Brown <Ralim@Ralimtek.com>
2021-12-21 22:07:07 +1100

    Add settings options

---
+++ library/compression.go
@@ -0,0 +1,8 @@
+package library
+
+// Compression handles compressing files using the existingnsz tooling
+// It runs a single file compression at a time in the background
+
+func (lib *Library) compressionWorker() {
+	//Dequeue any requests off the queue and run the compression
+}
