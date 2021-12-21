3705791afb9fe77fbf5409d458643395361ad0d3
Ben V. Brown <Ralim@Ralimtek.com>
2021-12-21 17:20:25 +1100

    More testing of ftp auth

--- server/virtualftp/fltp_test.go
+++ server/virtualftp/fltp_test.go
@@ -50,3 +50,64 @@ func TestAuthPutFile(t *testing.T) {
 	}
 
 }
+
+func TestAuthCheckPasswd(t *testing.T) {
+
+	temp_folder, err := os.MkdirTemp("", "unit_test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(temp_folder)
+	setting := settings.NewSettings(path.Join(temp_folder, "settings.json"))
+	setting.UploadingAllowed = false
+	driver := NewDriver(nil, setting)
+	ctx := &ftpserver.Context{
+		Sess: &ftpserver.Session{
+			Data: make(map[string]interface{}),
+		},
+	}
+	//Should work if anon is on
+	setting.AllowAnonFTP = false
+	ok, _ := driver.CheckPasswd(ctx, "", "")
+
+	if ok {
+		t.Error("should always allow anonftp")
+	}
+	//test fails if anon is off and no user accounts
+	setting.AllowAnonFTP = false
+	ok, _ = driver.CheckPasswd(ctx, "", "")
+	if ok {
+		t.Error("should fail if anon is off, and no user accounts")
+	}
+	//Should work if user exists
+	setting.Users = []settings.AuthUser{{Username: "test", Password: "testPass", AllowFTP: true, AllowUpload: true}}
+	setting.AllowAnonFTP = false
+	ok, _ = driver.CheckPasswd(ctx, "test", "testPass")
+	if !ok {
+		t.Error("should allow valid user")
+	}
+	if value, ok := ctx.Sess.Data["uploadAllowed"]; !ok || !value.(bool) {
+		t.Error("Should set the upload status")
+	}
+
+	setting.Users = []settings.AuthUser{{Username: "test", Password: "testPass", AllowFTP: true, AllowUpload: false}}
+	setting.AllowAnonFTP = false
+	ok, _ = driver.CheckPasswd(ctx, "test", "testPass")
+	if !ok {
+		t.Error("should allow valid user")
+	}
+	if value, ok := ctx.Sess.Data["uploadAllowed"]; !ok || value.(bool) {
+		t.Error("Should set the upload status")
+	}
+
+	setting.Users = []settings.AuthUser{{Username: "test", Password: "lololoo", AllowFTP: true, AllowUpload: true}}
+	setting.AllowAnonFTP = false
+	ok, _ = driver.CheckPasswd(ctx, "test", "testPass")
+	if ok {
+		t.Error("should block invalid user")
+	}
+	if value, ok := ctx.Sess.Data["uploadAllowed"]; !ok || value.(bool) {
+		t.Error("Should set the upload status")
+	}
+
+}
