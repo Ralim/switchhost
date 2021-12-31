a53fb11c1f297d797ab6d53bc1469cd53ce1e824
Ben V. Brown <Ralim@Ralimtek.com>
2021-12-31 16:04:37 +1100

    ctr

---
+++ formats/AESCTR/aesctr_encrypter.go
@@ -0,0 +1,62 @@
+package aesctr
+
+import (
+	"crypto/aes"
+	"errors"
+	"fmt"
+	"io"
+)
+
+// AESCTR is a stream wrapper that allows either encryption or decrytion around a stream
+// In decryption mode, it wraps a reader and exposes a new reader that will transparently read from the wrapped stream, and perform the decode on it
+
+//As we need to seek around in the stream, the golang default doesnt work for us
+
+type AESCTREncrypter struct {
+	sourceStream io.Reader
+
+	sourceKey []byte
+
+	counter *ctr
+}
+
+func NewAESCTREncrypter(reader io.Reader, key, prefix, postfix []byte) (*AESCTREncrypter, error) {
+	aesBlockCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	return &AESCTREncrypter{
+		sourceStream: reader,
+		sourceKey:    key,
+		counter:      Newctr(aesBlockCipher, aesBlockCipher.BlockSize(), prefix, postfix),
+	}, nil
+}
+
+func (A *AESCTREncrypter) Seek(i uint64) {
+	A.counter.Seek(i)
+}
+func (A *AESCTREncrypter) Read(p []byte) (int, error) {
+	//Read up to N bytes out, block aligned
+	blockSize := A.counter.Blocksize
+	n := len(p)
+	if n%blockSize != 0 {
+		fmt.Println("PADDDDD")
+		n -= (n % blockSize)
+	}
+	read, err := A.sourceStream.Read(p)
+	if read == 0 {
+		return read, fmt.Errorf("AES CTR Decrypter: read - %w", err)
+	}
+	// One issue to handle when time permits is read != n, and its no longger block aligned
+	if read != n {
+		if read%blockSize != 0 {
+			fmt.Println("read unalign", read, n)
+			return 0, errors.New("unhandled, read not of aes block size")
+		}
+		n = read
+	}
+	A.counter.XORKeyStream(p, p)
+
+	return n, err
+
+}
