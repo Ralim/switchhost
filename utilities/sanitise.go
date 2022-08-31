07382eaaf202712f94bcf2693d6126b4dccb7ff5
Ben V. Brown <Ralim@Ralimtek.com>
2022-08-31 21:40:42 +1000

    sanitise out newlines

--- utilities/sanitise.go
+++ utilities/sanitise.go
@@ -4,8 +4,8 @@ package utilities
 // Mostly removing slashes and trademark/copyright icons and such
 // Things that are "ugly" and dont contribute useful information
 
-//And : breaks some certain software when browsing
-var BadRunes map[rune]bool = map[rune]bool{'/': false, '™': false, '®': false, '©': false, '\\': false, ':': false}
+// And : breaks some certain software when browsing
+var BadRunes map[rune]bool = map[rune]bool{'/': false, '™': false, '®': false, '©': false, '\\': false, ':': false, '\n': false, '\r': false}
 
 func CleanName(s string) string {
 	out := ""
