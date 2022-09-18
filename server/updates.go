6888da1d3c3a5ed25f2de05668fc4855f13be28d
Ben V. Brown <Ralim@Ralimtek.com>
2022-09-18 12:19:23 +1000

    Update updates.go

--- server/updates.go
+++ server/updates.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (server *Server) handleServingUpdatesList(respWriter http.ResponseWriter, req *http.Request) {
-
+	respWriter.Header().Set("Content-Type", "application/json")
 	updates := server.library.GetGamesNeedingUpdate()
 	data, _ := json.MarshalIndent(updates, "", "  ")
 	_, _ = respWriter.Write(data)
